52361c99737da760474554cbb04487c8b6f1d9d5
Leandro Antunes <lele.tune@gmail.com>
2022-06-07 20:31:03 -0300

    Componente: Sidebar Finalizado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -6,12 +8,17 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/leandrotune.png" />
+
                 <strong>Leandro Antunes</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
