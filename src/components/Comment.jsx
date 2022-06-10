c8b86338ff37001a152f46c10387d25849651e28
Leandro Antunes <lele.tune@gmail.com>
2022-06-10 15:22:32 -0300

    Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/leandrotune.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Leandro Antunes</strong>
+                            <time title="08 de junho às 18:09h" dateTime="2022-05-08 18:09:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp size={20} />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
