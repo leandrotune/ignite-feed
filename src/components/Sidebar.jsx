bdcd40461c90b2baac2b743a904a693f269a27a8
Leandro Antunes <lele.tune@gmail.com>
2022-06-11 14:08:38 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -8,7 +9,7 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/leandrotune.png" />
+                <Avatar src="https://github.com/leandrotune.png" />
 
                 <strong>Leandro Antunes</strong>
                 <span>Web Developer</span>
