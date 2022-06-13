a8de71460ea2660fea2d365a4d1e290ccac2f6b7
Leandro Antunes <lele.tune@gmail.com>
2022-06-13 13:43:57 -0300

    Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,41 @@ import './global.css'
 
 import styles from './App.module.css'
 
+// author: { avatarURL: "", name: "", role: ""}
+// publishedAt: date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/leandro.tune.png',
+      name: 'Leandro Antunes',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-13 13:17:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Developer full stack'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-12 13:20:00'),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -15,17 +50,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Leandro Antunes"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure error inventore eveniet, cupiditate illo sequi impedit facere nemo modi accusamus officia aut deleniti voluptatum quaerat sint voluptatem assumenda culpa aliquam.
-            )"
-          />
-
-          <Post
-            author="Leandro Antunes"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure error inventore eveniet, cupiditate illo sequi impedit facere nemo modi accusamus officia aut deleniti voluptatum quaerat sint voluptatem assumenda culpa aliquam.
-            )"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
