6f11e9c0013767f7d7892a7f3e15f28a570a73b7
Leandro Antunes <lele.tune@gmail.com>
2022-06-16 16:34:55 -0300

    alterando os arquivos de pasta

---
+++ ignite-feed_js/src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50" />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/leandrotune.png" />
+
+        <strong>Leandro Antunes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
