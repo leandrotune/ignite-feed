eb34f496aaab39bc72322c48da6292acacfaa7d3
Leandro Antunes <lele.tune@gmail.com>
2022-06-17 11:35:49 -0300

    Mudando o Projeto em TS

---
+++ ignite-feed_ts/src/App.tsx
@@ -0,0 +1,68 @@
+import { Header } from './components/Header'
+import { Post, PostProps } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+
+import './global.css'
+
+import styles from './App.module.css'
+
+interface Posts extends PostProps {
+  id: number;
+}
+const posts: Posts[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/leandrotune.png',
+      name: 'Leandro Antunes',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-13 13:17:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Developer full stack'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-12 13:20:00'),
+  },
+];
+
+
+export function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </>
+  )
+}
+
